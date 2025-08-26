4ee491aad62e5ee97af2e4250a34254da70312bf
1FahadShah <fahad999shah57@gmail.com>
2025-08-26 15:18:28 +0530

    Created get_connection() in connection.py

--- db/connection.py
+++ db/connection.py
@@ -1,3 +1,8 @@
 import sqlite3
 from pathlib import Path
 
+def get_connection(db_name="local_cache.db"):
+    db_path = Path(__file__).resolve().parent / db_name
+    connect = sqlite3.connect(db_path)
+    connect.row_factory = sqlite3.Row
+    return connect
