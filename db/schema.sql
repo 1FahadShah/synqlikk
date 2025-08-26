d97c5198f56b27abc53111e5ab72892ea6d17fa1
1FahadShah <fahad999shah57@gmail.com>
2025-08-26 14:11:18 +0530

    Created Users Table Schema

--- db/schema.sql
+++ db/schema.sql
@@ -7,12 +7,12 @@
 -- ============================================
 
 CREATE TABLE IF NOT EXISTS users (
-    id TEXT PRIMARY KAY,         -- UUID v4
+    id TEXT PRIMARY KEY,         -- UUID v4
     username TEXT UNIQUE NOT NULL,
     email TEXT UNIQUE,
     password_hash TEXT NOT NULL,
     created_at TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%SZ','now')),
     modified_at TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%SZ','now')),
-    is_deleted INTEGER DEFAULT 0
+    is_deleted INTEGER DEFULT 0
 );
 CREATE INDEX IF NOT EXISTS idx_users_username ON users(username)
