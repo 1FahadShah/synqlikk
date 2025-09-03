5f71c6b2a5280c12128b72f5d8c24ea6452ea312
1FahadShah <fahad999shah57@gmail.com>
2025-09-04 02:55:07 +0530

    Feat(cli/utils.py): Added colorama helpers for colored output

--- cli/utils.py
+++ cli/utils.py
@@ -1,4 +1,19 @@
 import uuid
+from colorama import init, Fore, Style
 
 def generate_uuid():
     return str(uuid.uuid4())
+
+init(autoreset=True)
+
+def print_success(msg):
+    print(Fore.GREEN + msg)
+
+def print_info(msg):
+    print(Fore.CYAN + msg)
+
+def print_warning(msg):
+    print(Fore.YELLOW + msg)
+
+def print_error(msg):
+    print(Fore.RED + msg)
